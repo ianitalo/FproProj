1774c6e19f1daa118eb399111754b5c97103749b
ianitalo <ianitalo56@hotmail.com>
2020-12-19 13:07:13 +0000

    FproProjCode

--- FproProjCode.py
+++ FproProjCode.py
@@ -1,2 +1,13 @@
 #FproProj
-                    
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+#Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
---
+++ untitled0.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 16 20:20:51 2020
+
+@author: jeffe
+"""
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
